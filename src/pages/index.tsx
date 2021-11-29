b2531c5a12ecbccc504a1bc85398e7d258b90b16
Leandro Camilotti <lecamilotti@gmail.com>
2021-11-29 19:17:58 +0100

    coments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
+
+// Client-side
+// Server-side
+// Static site generation
 
 interface HomeProps {
   product: {
@@ -35,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1K1DTYLtZYckhJ6WvlGITLhf");
 
   const product = {
@@ -49,5 +53,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, //24 hours
   };
 };
