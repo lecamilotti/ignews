b83befc1a0cb4332a0cc6d66eb42ea9160238402
Leandro Camilotti <lecamilotti@gmail.com>
2021-12-01 19:54:18 +0100

    next auth client

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,24 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-
-  return isUserLoggedIn ? (
+  const [session] = useSession();
+  console.log(session);
+  return session ? (
     <button className={styles.signInButton} type="button">
       <FaGithub color="#04d361" />
       Leandro Camilotti
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,11 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import Providers from "next-auth/providers";
+
 
 export default NextAuth( { // Configure one or more authentication providers
   providers: [
-    GithubProvider(
-      { clientId: process.env.GITHUB_CLIENT_ID, clientSecret: process.env.GITHUB_CLIENT_SECRET }
+    Providers.GitHub(
+      { clientId: process.env.GITHUB_CLIENT_ID, clientSecret: process.env.GITHUB_CLIENT_SECRET, scope: 'read:user' }
     ),
     // ...add more providers here
   ] } );
