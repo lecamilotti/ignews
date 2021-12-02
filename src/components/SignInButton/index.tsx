c8ad431a38a93d453decf92b22e5260bf8d4c8b1
Leandro Camilotti <lecamilotti@gmail.com>
2021-12-02 13:29:02 +0100

    authentication with nextAuth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,16 +1,20 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import { signIn, useSession } from "next-auth/client";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const [session] = useSession();
+  const { data: session } = useSession();
   console.log(session);
   return session ? (
-    <button className={styles.signInButton} type="button">
+    <button
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Leandro Camilotti
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
