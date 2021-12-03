1998792b02b4c83390ec136d28b232562f558134
Leandro Camilotti <lecamilotti@gmail.com>
2021-12-03 15:55:21 +0100

    fauna DB

--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,3 +1,3 @@
 import { Client } from 'faunadb';
 
-export const fauna = new Client( { secret: process.env.FAUNADB_KEY } );
+export const fauna = new Client( { secret: process.env.FAUNADB_KEY, domain: "db.eu.fauna.com" } );
