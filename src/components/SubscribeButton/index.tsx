c59bc9a62ba7933c29a7e40c98b65f8ff3bacd45
Leandro Camilotti <lecamilotti@gmail.com>
2021-12-04 08:15:27 +0100

    checkout session stripe and subscribe button

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,11 +1,26 @@
+import { signIn, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+  function handleSubscribe() {
+    if (!session) {
+      alert("You must be logged in to subscribe");
+      signIn("github");
+      return;
+    }
+    // check out session
+    // if logged in, subscribe
+  }
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
