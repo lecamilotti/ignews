747a68e77e504d9509514f74580114a9702d49b9
Leandro Camilotti <lecamilotti@gmail.com>
2021-12-06 16:32:34 +0100

    saving the subscription on faunaDB

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -3,35 +3,20 @@ import { query as q } from "faunadb";
 import { fauna } from "../../../services/fauna";
 import { stripe } from "../../../services/stripe";
 
-export async function saveSubscription (
-  subscriptionId: string,
-  crustomerId: string,
-) {
- const userRef = await fauna.query(
-   q.Select(
-     'ref',
-     q.Get(
-      q.Match(
-        q.Index('users_by_customer_id'),
-        crustomerId
-       )
-     )
-   )
- )
+export async function saveSubscription( subscriptionId : string, crustomerId : string, ) {
 
- const subscription = await stripe.subscriptions.retrieve(subscriptionId)
-         const subscriptionData = {
-           id: subscription.id,
-           userId: userRef,
-           status: subscription.status,
-           price_id: subscription.items.data[0].price.id,
-         }
+  const userRef = await fauna.query( q.Select( "ref", q.Get( q.Match( q.Index( 'user_by_stripe_customer_id' ), crustomerId ) ) ) );
 
- await fauna.query(
-   q.Create(
-     q.Collection('subscriptions'),
-     { data: subscriptionData }
-   )
- )
+  const subscription = await stripe.subscriptions.retrieve( subscriptionId );
+  const subscriptionData = {
+
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[ 0 ].price.id
+
+  };
+
+  await fauna.query( q.Create( q.Collection( 'subscriptions' ), { data: subscriptionData } ) );
 
 }
