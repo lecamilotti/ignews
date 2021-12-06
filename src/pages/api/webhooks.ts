8750dee7c2fd080392fc97c65ae580850a783be0
Leandro Camilotti <lecamilotti@gmail.com>
2021-12-06 16:13:59 +0100

    webhooks and managing subscription

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,12 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
 
   for await (const chunk of readable) {
-    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+    chunks.push(
+      typeof chunk === "string" ? Buffer.from(chunk) : chunk
+      );
   }
   return Buffer.concat(chunks);
 }
@@ -14,30 +18,48 @@ async function buffer(readable: Readable) {
 export const config = {
   api: {
     bodyParser: false,
-  },
-};
+  }
+}
 const relevantEvents = new Set(["checkout.session.completed"]);
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    const buf = await buffer(req);
-    const secret = req.headers["stripe-signature"];
+    const buf = await buffer(req)
+    const secret = req.headers["stripe-signature"]
 
     let event: Stripe.Event;
 
     try {
-      event = Stripe.webhooks.constructEvent(
+      event = stripe.webhooks.constructEvent(
         buf,
         secret,
         process.env.STRIPE_WEBHOOK_SECRET
       );
     } catch (err) {
-      return res.status(400).send("Webhook error: ${err.message}");
+      return res.status(400).send(`Webhook error: ${err.message}`);
     }
+    
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log(`Received event` event);
+     try {
+       switch (type) {
+         case 'checkout.session.completed':
+           const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+          await saveSubscription(
+            checkoutSession.subscription.toString(),
+            checkoutSession.customer.toString()
+          )
+
+
+           break;
+          default:
+            throw new Error(`Unexpected event type: ${type}`);
+       }
+     }catch (err) {
+       return res.json({ error: 'Webhook handler failed.'})
+     }
 
       // Handle the event
     }
