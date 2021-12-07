63b00be2f42160aa290b6cf820df041d5718cce7
Leandro Camilotti <lecamilotti@gmail.com>
2021-12-07 10:40:13 +0100

    creating post page and prismic cms

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,27 @@
+import styles from './styles.module.scss';
+
+export default function Posts() {
+  return (
+    <>
+    <Head>
+      <title>Posts | TechNews </title>
+    </Head>
+      <main>
+      <div>
+        <a>
+          <time>21 april 2021</time>
+          <strong>Creating a Monorepo with Lerna & yarn workspaces</strong>
+          <p>In this guide, you will learn to create a Monorepo to manage multiple packages</p>
+          
+        </a>
+      </div>
+
+      </main>
+
+
+
+
+    </>
+
+  );
+}
