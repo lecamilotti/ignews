053ec2c7355a4dd35512f30a98e53f2f4e3bd6ef
Leandro Camilotti <lecamilotti@gmail.com>
2021-12-07 11:00:25 +0100

    styling post page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -7,13 +7,27 @@ export default function Posts() {
     <Head>
       <title>Posts | TechNews </title>
     </Head>
-      <main>
-      <div>
-        <a>
+      <main className={styles.container}>
+      <div className={styles.posts}>
+        <a href="#">
           <time>21 april 2021</time>
           <strong>Creating a Monorepo with Lerna & yarn workspaces</strong>
           <p>In this guide, you will learn to create a Monorepo to manage multiple packages</p>
-          
+        </a>
+        <a href="#">
+          <time>21 april 2021</time>
+          <strong>Creating a Monorepo with Lerna & yarn workspaces</strong>
+          <p>In this guide, you will learn to create a Monorepo to manage multiple packages</p>
+        </a>
+        <a href="#">
+          <time>21 april 2021</time>
+          <strong>Creating a Monorepo with Lerna & yarn workspaces</strong>
+          <p>In this guide, you will learn to create a Monorepo to manage multiple packages</p>
+        </a>
+        <a href="#">
+          <time>21 april 2021</time>
+          <strong>Creating a Monorepo with Lerna & yarn workspaces</strong>
+          <p>In this guide, you will learn to create a Monorepo to manage multiple packages</p>
         </a>
       </div>
 
