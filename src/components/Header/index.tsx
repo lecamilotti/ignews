b617b287a619bfe0818daf693f313ec15425cd2a
Leandro Camilotti <lecamilotti@gmail.com>
2021-12-13 12:15:16 +0100

    prefetch and load the links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
@@ -7,8 +9,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="logo" />
         <nav>
+          <Link href="/">
           <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts" prefetch>
           <a>Posts</a>
+          </Link>
         </nav>
         <SignInButton />
       </div>
