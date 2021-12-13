17dcca4d164b1728165df88779361621eb572658
Leandro Camilotti <lecamilotti@gmail.com>
2021-12-13 12:33:23 +0100

    cloneElement with hooks for change the props of an anchor

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,19 +2,21 @@ import Link from 'next/link';
 
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="logo" />
         <nav>
-          <Link href="/">
-          <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active} href="/">
+          <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
           <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
